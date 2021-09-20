52021c5a9de1ae77b55b1a3933caae4265476fb3
Peter Brady <ptb@lanl.gov>
2021-09-20 16:13:41 -0600

    Added eigenvalue simulations as a constraint
    Initial work on reading a lua input file that contains `Simulations`
    and `Constraints` tables.  These tables should be looped over and processed.
    Much of the logic needs to be embedded in the table itself.  The exact manner
    of how things are handled will likely change when we move to MPI/Coroutine
    based solutions

---
+++ driver.lua
@@ -0,0 +1,143 @@
+local inspect = require "inspect"
+
+local eigenvalue_base = {
+    {
+        mesh = {
+            index_extents = {21},
+            domain_bounds = {1}
+        },
+        shapes = {
+            {
+                type = "yz_rect",
+                psi = 0.001,
+                normal = 1,
+                boundary_condition = "dirichlet"
+            },
+            {
+                type = "yz_rect",
+                psi = 0.9,
+                normal = -1,
+                boundary_condition = "floating"
+            }
+        },
+        scheme = {
+            order = 1,
+            type = "E2-poly",
+            floating_alpha = {13 / 100, 7 / 50, 3 / 20, 4 / 25, 17 / 100, 9 / 50},
+            dirichlet_alpha = {3 / 25, 13 / 100, 7 / 50}
+        },
+        system = {
+            type = "eigenvalues"
+        }
+    }
+}
+
+local function simple_copy(obj)
+    if type(obj) ~= "table" then
+        return obj
+    end
+    local res = {}
+    for k, v in pairs(obj) do
+        res[simple_copy(k)] = simple_copy(v)
+    end
+    return res
+end
+
+local function nested_set(tbl, v, x, ...)
+    if ... == nil then
+        tbl[x] = v
+    else
+        return nested_set(tbl[x], v, ...)
+    end
+end
+
+local function add_entries(tbl, vs, ...)
+    local out = {}
+    for _, t in ipairs(tbl) do
+        for _, v in ipairs(vs) do
+            local i = #out + 1
+            out[i] = simple_copy(t)
+            nested_set(out[i], v, ...)
+        end
+    end
+    return out
+end
+
+local psi_values = {1e-12, 1e-6, 1e-3, 1e-2, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 - 1e-8}
+
+-- only add a bunch of psi constraints on the left boundary since we're only
+Constraints = {
+    {
+        simulations = add_entries(
+            add_entries(eigenvalue_base, {21, 31, 41, 51, 61, 71, 81, 91, 101, 111}, "mesh", "index_extents", 1),
+            psi_values,
+            "shapes",
+            1,
+            "psi"
+        ),
+        set_values = function(lst)
+            assert(#lst >= 9)
+            for _, t in ipairs(Constraints[1].simulations) do
+                for i = 1, 6 do
+                    t.scheme.floating_alpha[i] = lst[i]
+                end
+                for i = 1, 3 do
+                    t.scheme.dirichlet_alpha[i] = lst[i + 6]
+                end
+            end
+        end,
+        set_result = function(i, lst)
+            Constraints[1].simulations[i].result = lst[1]
+        end,
+        aggregate_result = function()
+            local result = -math.huge
+            for _, v in ipairs(Constraints[1].simulations) do
+                result = math.max(result, v.result)
+            end
+            return result
+        end,
+        debug = function(i)
+            print("tbl: ", i, inspect(Constraints[1].simulations[i]))
+        end
+    }
+}
+
+Simulation = {
+    mesh = {
+        index_extents = {51},
+        domain_bounds = {2}
+    },
+    domain_boundaries = {
+        xmin = "dirichlet"
+    },
+    shapes = {
+        {
+            type = "yz_rect",
+            psi = 0.99,
+            normal = 1,
+            boundary_condition = "dirichlet"
+        }
+    },
+    scheme = {
+        order = 1,
+        type = "E2-poly",
+        alpha = {}
+    },
+    system = {
+        type = "scalar wave",
+        center = {-1},
+        radius = 0,
+        max_error = 2.0
+    },
+    integrator = {
+        type = "rk4"
+    },
+    step_controller = {
+        max_time = 100,
+        --max_step = 2,
+        cfl = {
+            hyperbolic = 0.8,
+            parabolic = 0.2
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,134 @@
 #include <cmath>
+
 #include <fmt/core.h>
+#include <fmt/ranges.h>
+
+#include <functional>
 #include <limits>
 #include <nlopt.hpp>
+#include <shoccs.hpp>
+#include <sol/sol.hpp>
+#include <span>
 #include <vector>
-#include <functional>
 
-double my_func(unsigned n, const double* x, double* grad, void* my_func_data)
+#include <cxxopts.hpp>
+
+#include <spdlog/spdlog.h>
+
+double objective(unsigned n, const double* x, double*, void* data)
 {
-    if (grad) {
-        grad[0] = 0.0;
-        grad[1] = 0.5 / std::sqrt(x[1]);
+    sol::table& tbl = *reinterpret_cast<sol::table*>(data);
+
+    double max_time = tbl["step_controller"]["max_time"];
+    for (unsigned i = 0; i < n; i++) { tbl["scheme"]["alpha"][i + 1] = x[i]; }
+
+    auto result = ccs::simulation_run(tbl);
+    assert(result);
+
+    auto&& [time, error, _] = *result;
+
+    tbl["constraint"] = max_time - time;
+
+    if (time < max_time) {
+        return 10 * time / max_time;
+    } else {
+        return 10 - std::log(error);
     }
-    return std::sqrt(x[1]);
 }
 
-struct my_constraint_data {
-    double a, b;
-};
-
-double my_constraint(unsigned n, const double* x, double* grad, void* data)
+double constraint(unsigned n, const double* x, double*, void* data)
 {
-    auto [a, b] = *reinterpret_cast<my_constraint_data*>(data);
+    sol::table& t = *reinterpret_cast<sol::table*>(data);
 
-    if (grad) {
-        grad[0] = 3 * a * (a * x[0] + b) * (a * x[0] + b);
-        grad[1] = -1.0;
+    std::span<const double> params{x, n};
+    t["set_values"](params);
+
+    for (int i = 1; t["simulations"][i].valid(); i++) {
+        t["debug"](i);
+        auto result = ccs::simulation_run(t["simulations"][i]);
+        assert(result);
+        t["set_result"](i, *result);
     }
-    return (a * x[0] + b) * (a * x[0] + b) * (a * x[0] + b) - x[1];
+
+    return t["aggregate_result"]();
 }
 
-template <typename T>
-double wrap_lambda(unsigned n, const double* x, double* grad, void* data)
+int main(int argc, char* argv[])
 {
-    T* l = reinterpret_cast<T*>(data);
-    return std::invoke(*l, n, x, grad);
-}
 
-int main()
-{
-    nlopt::opt opt(nlopt::LN_SBPLX, 2);
+    cxxopts::Options options("fido", "Run the finite-difference optimizer a given input");
 
-    std::vector<double> lower_bounds{-5, 0};
-    std::vector<double> upper_bounds{5, 10};
+    // clang-format off
+    options.add_options()
+        ("input-file", "Main lua input file", cxxopts::value<std::string>())
+        ("help", "Print usage");
+    // clang-format on
+    options.parse_positional("input-file");
 
-    opt.set_lower_bounds(lower_bounds);
-    opt.set_upper_bounds(upper_bounds);
+    auto result = options.parse(argc, argv);
 
-    int count {};
-    auto my_l_func = [&count](unsigned n, const double* x, double* grad) {
-        ++count;
-        if (grad) {
-            grad[0] = 0.0;
-            grad[1] = 0.5 / std::sqrt(x[1]);
-        }
-        return std::sqrt(x[1]);
-    };
-    
-    opt.set_min_objective(wrap_lambda<decltype(my_l_func)>, &my_l_func);
+    if (result.count("help") || result.arguments().size() == 0) {
+        std::cout << options.help() << '\n';
+        return 0;
+    }
 
-    auto cd = std::vector<my_constraint_data>{{2.0, 0}, {-1, 1}};
-    //opt.add_inequality_constraint(my_constraint, &cd[0], 1e-8);
-    //opt.add_inequality_constraint(my_constraint, &cd[1], 1e-8);
+    sol::state lua;
+    lua.open_libraries(
+        sol::lib::base, sol::lib::string, sol::lib::package, sol::lib::math);
+
+    if (!result.count("input-file")) {
+        spdlog::error("input file must be specified");
+        return 1;
+    }
+
+    lua.script_file(result["input-file"].as<std::string>());
+
+    if (!lua["Simulation"].valid()) {
+        spdlog::error("top level `Simulation` table must be specified");
+        return 1;
+    }
+    sol::table tbl = lua["Simulation"];
+
+    nlopt::opt opt(nlopt::LN_COBYLA, 9);
+
+    // std::vector<double> lower_bounds{-5, 0};
+    // std::vector<double> upper_bounds{5, 10};
+
+    // opt.set_lower_bounds(lower_bounds);
+    // opt.set_upper_bounds(upper_bounds);
+
+    // int count{};
+    // auto my_l_func = [&count](unsigned n, const double* x, double* grad) {
+    //     ++count;
+    //     if (grad) {
+    //         grad[0] = 0.0;
+    //         grad[1] = 0.5 / std::sqrt(x[1]);
+    //     }
+    //     return std::sqrt(x[1]);
+    // };
+
+    opt.set_max_objective(objective, &tbl);
+
+    if (!lua["Constraints"][1].valid()) {
+        spdlog::error("top level `Constraints` table must be specified");
+        return 1;
+    }
+
+    sol::table constraints = lua["Constraints"][1];
+    opt.add_inequality_constraint(constraint, &constraints, 0.0);
+    // opt.add_inequality_constraint(my_constraint, &cd[1], 1e-8);
 
     opt.set_xtol_rel(1e-5);
-    //opt.set_xtol_abs(1e-6);
+    opt.set_xtol_abs(1e-8);
+    opt.set_maxeval(20);
+    opt.set_initial_step(0.1);
 
-    auto x = std::vector{1.234, 5.678};
-    double minf;
+    auto x = std::vector(9, 0.0);
+    double maxval;
 
-    auto result = opt.optimize(x, minf);
-    fmt::print("found minimum in {} evaluations at f({}, {}) = {}\n", opt.get_numevals(), x[0], x[1], minf);
+    auto opt_res = opt.optimize(x, maxval);
+    fmt::print("found maximum in {} evaluations at f({}) = {}\n",
+               opt.get_numevals(),
+               fmt::join(x, ", "),
+               maxval);
 }
