9e135870f1efece4561a71cf0894cacc947b1593
Peter Brady <ptb@lanl.gov>
2023-03-23 16:33:38 -0600

    Update shoccs recipe

--- spack-repo/packages/shoccs/package.py
+++ spack-repo/packages/shoccs/package.py
@@ -32,18 +32,19 @@ class Shoccs(CMakePackage):
     maintainers = ['pbrady']
 
     version('develop', branch='main')
-    version('2022-04', commit='90f7ad1277e7e746ee506b33f530b016240b094e', preferred=True)
+    version('2023-03', commit='ea1ef2dc1c7656fe5b685a355ec4e771318c063f', preferred=True)
+    version('2022-04', commit='90f7ad1277e7e746ee506b33f530b016240b094e')
     version('2022-02', commit='2203cab4e2bc69e365f6f8a780a47ac33d3108fa')
 
     depends_on('lua-sol2')
     depends_on('cmake@3.16:')
     depends_on('range-v3@0.11:')
     depends_on('pugixml')
-    depends_on('fmt@8.0.1')
-    depends_on('spdlog@1.9:')
+    depends_on('fmt@8:')
+    depends_on('spdlog@1.9: +fmt_external')
     depends_on('cxxopts@3:')
     depends_on('boost cxxstd=2a')
-    depends_on('mkl')
+    depends_on('lapackpp')
     depends_on('catch2@3:')
 
     def cmake_args(self):
